99c346f18095241a083018eb3e9d50415cde3850
Tezcat5 <vicpero96@gmail.com>
2025-05-26 11:27:02 +0200

    Scriptable script

---
+++ Assets/Scripts/Scriptable.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "New Item", menuName = "Item/New Item")]
+
+public class Scriptable : ScriptableObject
+
+{
+    public string itemName;
+
+    public int itemPrice;
+    
+    public Sprite itemSprite;
+}
